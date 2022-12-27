16ae53655ea869306b6721928e32aecb68bcad77
FihCapua <fihcapua@gmail.com>
2022-12-27 12:31:25 -0300

    feat: add theme providers

--- src/components/theme/globalstyles.tsx
+++ src/components/theme/globalstyles.tsx
@@ -6,8 +6,7 @@ const GlobalStyle = createGlobalStyle`
     color: ${({ theme }) => theme.colors.primary};
     padding: 0;
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    font-family: ${({ theme }) => theme.fontFamily};
   }
 
   a {
@@ -18,6 +17,18 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
   }
-`
+
+  /* Full height layout */ 
+  html, body {
+    display: flex;
+    min-height: 100vh;
+    width: 100%;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+  }
+`;
 
 export default GlobalStyle
---
+++ types/styled-components.d.ts
@@ -0,0 +1,7 @@
+import { theme } from '../src/components/theme/theme';
+
+type Theme = typeof theme;
+
+declare module 'styled-components' {
+  export interface DefaultTheme extends Theme {}
+}
