97f7c8ac7b1d7851537b112e6f0e461f2feb6b7f
FihCapua <fihcapua@gmail.com>
2022-12-30 00:32:32 -0300

    feat: include new features in form like integration api and animations

--- src/components/patterns/FormCadastro/index.tsx
+++ src/components/patterns/FormCadastro/index.tsx
@@ -2,11 +2,24 @@ import React, { useState } from "react";
 import { Button } from "../../common/Button";
 import { TextField } from "../../forms/TextField";
 import { Typography } from "../../foundation/Typography";
+import Lottie from "lottie-react";
+import successAnimation from "./animation/success.json";
+import errorAnimation from "./animation/error.json";
+
+const formState = {
+  DEFAULT: "DEFAULT",
+  LOADING: "LOADING",
+  DONE: "DONE",
+  ERROR: "ERROR",
+};
 
 export function FormCadastro() {
+  const [isFormSubmited, setIsFormSubmited] = useState(false);
+  const [submissionStatus, setSubmissionStatus] = useState(formState.DEFAULT);
+
   const [userInfo, setUserInfo] = useState({
-    usuario: "JoazinhoGamePlay",
-    email: "joazinho@gameplay.com",
+    user: "",
+    name: "",
   });
 
   /* O fieldName serviria pra passar a variável dinâmica pra cada valor do input
@@ -20,15 +33,54 @@ export function FormCadastro() {
     });
   }; */
 
-  const isFormValid =
-    userInfo.usuario.length === 0 || userInfo.usuario.length === 0;
+  const isFormValid = userInfo.user.length === 0 || userInfo.name.length === 0;
+
+  const styleLottieDiv = {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: "10px",
+  };
+
+  const styleLottieAnimation = {
+    width: "55px",
+    height: "55px",
+  };
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
 
-        console.log("Formulário pronto para cadastrar");
+        setIsFormSubmited(true);
+
+        // Data Transfer Object
+        const userDTO = {
+          username: userInfo.user,
+          name: userInfo.name,
+        };
+
+        fetch("https://instalura-api.vercel.app/api/users", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(userDTO),
+        })
+          .then((res) => {
+            if (res.ok) {
+              return res.json();
+            }
+
+            throw new Error("Não foi possível cadastrar seu usuário agora =(");
+          })
+          .then((res) => {
+            setSubmissionStatus(formState.DONE);
+            console.log(res);
+          })
+          .catch((error) => {
+            setSubmissionStatus(formState.ERROR);
+            console.log(error);
+          });
       }}
     >
       <Typography variant="title" as="h1" color="tertiary">
@@ -40,28 +92,28 @@ export function FormCadastro() {
       </Typography>
 
       <TextField
-        type="email"
-        name="email"
-        placeholder="Email"
-        value={userInfo.email}
+        type="usuario"
+        name="usuario"
+        placeholder="Usuário"
+        value={userInfo.user}
         onChange={(event) => {
           console.log(userInfo);
           setUserInfo({
             ...userInfo,
-            email: event.target.value,
+            user: event.target.value,
           });
         }}
       />
       <TextField
         type="text"
-        name="user"
-        value={userInfo.usuario}
-        placeholder="Usuário"
+        name="name"
+        placeholder="Nome"
+        value={userInfo.name}
         onChange={(event) => {
           console.log(userInfo);
           setUserInfo({
             ...userInfo,
-            usuario: event.target.value,
+            name: event.target.value,
           });
         }}
       />
@@ -69,6 +121,30 @@ export function FormCadastro() {
       <Button fullWidth ghost={false} disabled={isFormValid} type="submit">
         Cadastrar
       </Button>
+
+      {isFormSubmited && submissionStatus === formState.DONE && (
+        <div style={styleLottieDiv}>
+          <Lottie
+            style={styleLottieAnimation}
+            className="lottie-container basic"
+            animationData={successAnimation}
+            loop={false}
+          />
+          <p>Dados enviados com sucesso</p>
+        </div>
+      )}
+
+      {isFormSubmited && submissionStatus === formState.ERROR && (
+        <div style={styleLottieDiv}>
+          <Lottie
+            style={styleLottieAnimation}
+            className="lottie-container basic"
+            animationData={errorAnimation}
+            loop={false}
+          />
+          <p>Erro no envio dos dados</p>
+        </div>
+      )}
     </form>
   );
 }
